c3f8bae8887517d0fe9d90b0c73a5a51b43721aa
omershacham <Omer@cnvrg.io>
2019-12-22 09:53:05 +0200

    init

---
+++ flask-2nd/src/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    cat = requests.get("https://api.thecatapi.com/v1/images/search?size=full").json()[0]
+    return render_template('index.html', url=cat.get("url"))
+if __name__ == '__main__':
+    app.run()
